3c1fd49d352a76069f657368bc3a49f15f4fbde6
dim <dim@VB>
2020-12-16 12:06:09 +0100

    gui test

---
+++ project/src/main/java/GUI/guiTest.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import settings.SixArmBoard;
+
+public class guiTest {
+
+	public static void main(String[] args) {
+		SixArmBoard board = new SixArmBoard();
+		SixArmBoardGUI g = new SixArmBoardGUI(board);
+	}
+
+}
