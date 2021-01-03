47d880e62c8e4f1defa506333d89fe188837e7d4
Mateusz Tofil <mateusz.tofil52@gmail.com>
2021-01-03 17:33:38 +0100

    Test FieldModelTEST

--- project/src/test/java/model/FieldModelTEST.java
+++ project/src/test/java/model/FieldModelTEST.java
@@ -1,4 +1,53 @@
 package model;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class FieldModelTEST {
+    FieldModel fieldModel;
+
+    @Before
+    public void init() {
+        fieldModel = new FieldModel();
+    }
+
+    @Test
+    public void isTakenTest() {
+        assertFalse(fieldModel.isTaken());
+        fieldModel.setFieldColor(ColorsFor2Players.GREEN);
+        assertTrue(fieldModel.isTaken());
+    }
+
+    @Test
+    public void setFieldColorTest() {
+        assertNull(fieldModel.getColor());
+        fieldModel.setFieldColor(ColorsFor2Players.BLUE);
+        assertNotNull(fieldModel.getColor());
+        assertEquals(ColorsFor2Players.BLUE, fieldModel.getColor());
+    }
+
+    @Test
+    public void setFieldFreeTest() {
+        fieldModel.setFieldColor(ColorsFor4Players.YELLOW);
+        assertTrue(fieldModel.isTaken());
+        fieldModel.setFieldFree();
+        assertFalse(fieldModel.isTaken());
+        assertEquals(State.FREE, fieldModel.getState());
+    }
+
+    @Test
+    public void getColorTest() {
+        fieldModel.setFieldColor(ColorsFor2Players.BLUE);
+        assertEquals(ColorsFor2Players.BLUE, fieldModel.getColor());
+    }
+
+    @Test
+    public void setFieldColorHintTest() {
+        fieldModel.setFieldColorHint(ColorsFor2Players.GREEN);
+        // synchronizedEnums - remember
+        assertNull(fieldModel.getColor());
+    }
+
 }
