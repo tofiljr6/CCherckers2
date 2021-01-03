a4df7a4d0e767231a49246b07f296b0975a18d69
Mateusz Tofil <mateusz.tofil52@gmail.com>
2021-01-03 17:03:40 +0100

    Test SixArmBoardTEST

--- project/src/test/java/settings/SixArmBoardTEST.java
+++ project/src/test/java/settings/SixArmBoardTEST.java
@@ -1,4 +1,25 @@
 package settings;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SixArmBoardTEST {
+    public static SixArmBoard sixArmBoard;
+
+    @Before
+    public void init() {
+        sixArmBoard = new SixArmBoard();
+    }
+
+    @Test
+    public void getXSizeTest() {
+        assertEquals(25, sixArmBoard.getXSize());
+    }
+
+    @Test
+    public void getYSizeTest() {
+        assertEquals(17, sixArmBoard.getYSize());
+    }
 }
